b4bc921b9d3716746c2282122da01a94eaf7590a
Kubilay İşen <isen.kubilay@gmail.com>
2020-05-31 17:53:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+def isInteger(s):
+  s = s.strip()
+  if (s[0] == "+" or s[0] == "-") and s[1:].isdigit():
+    return True
+  if s.isdigit():
+    return True
+  return False 
+def main():
+  s = input("Enter a string: ")
+  if isInteger(s):
+    print("That string represents an integer.")
+  else:
+    print("That string does not represent an integer.")
+
+if __name__ == "__main__":
+  main()
